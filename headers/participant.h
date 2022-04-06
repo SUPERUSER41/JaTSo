2f2217881533108cb91046f6528cad595d8821b9
Daneil Greaves <daneilgreaves@gmail.com>
2022-04-06 08:31:54 -0500

    create participant class

--- headers/participant.h
+++ headers/participant.h
@@ -6,41 +6,15 @@
 typedef struct Participant
 {
     int id;
-    char name[50];
-    char school[50];
+    char *name;
+    char *school;
     char gender;
-    Date dob;
-    struct Participant *next;
+    Date *dob;
+    void (*Register)(struct Participant *p, char *name, char *school, char gender, Date *dob;);
+    void (*Destroy)(struct Participant *p);
 } Participant;
 
-// Create a participant
-Participant *createParticipant(int id, char *name, char *school, char gender, Date *dob);
-
-// Destroy a participant
-void destroyParticipant(Participant *p);
-
-// Get all participants
-Participant *getAllParticipants();
-
-// Get a single participant by their id
-Participant *getParticipantById(int id);
-
-// Print a single participant
-void printParticipant(Participant *p);
-
-// Print all participants
-void printAllParticipants(Participant *participants);
-
-// Get participant age
-int getParticipantAge(int birthYear);
-
-// Calculate participant time
-void calculateParticipantTime();
-
-// Generate a unique id number
-int generateId(Participant *participants, Participant *p);
-
-// Add a participant to a linked list
-Participant *addParticipant(Participant **participants, Participant *p);
-
+static Participant InitParticipant();
+static void Register(Participant *p, char *name, char *school, char gender, Date *dob);
+static void Destroy(Participant *p);
 #endif
--- src/Participant.c
+++ src/Participant.c
@@ -1,77 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #include "../headers/participant.h"
 #include "../headers/date.h"
 
-int generateId(Participant *participants, Participant *p)
+Participant InitParticipant()
 {
-    int id = 0;
-    for (p = participants; p; p = p->next)
-    {
-        id = p->id;
-    }
-    return id + 1;
+    Participant p;
+    Date dob;
+    dob.day = 0;
+    dob.month = 0;
+    dob.year = 0;
+    p.id = 0;
+    p.name = "";
+    p.school = "";
+    p.gender = 'M';
+    p.dob = &dob;
+    p.Destroy = &Destroy;
+    p.Register = &Register;
 }
-
-Participant *createParticipant(int id, char *name, char *school, char gender, Date *dob)
+void Register(Participant *p, char *name, char *school, char gender, Date *dob)
 {
-    Participant *p = malloc(sizeof(Participant));
-    if (p != NULL)
-    {
-        p->id = id;
-        strcpy(p->name, name);
-        strcpy(p->school, school);
-        p->gender = gender;
-        p->dob = *dob;
-        p->next = NULL;
-    }
-    return p;
+    p->name = name;
+    p->school = school;
+    p->gender = gender;
+    p->dob = dob;
 }
-
-Participant *addParticipant(Participant **participants, Participant *p)
+void Destroy(Participant *p)
 {
-    p->next = *participants;
-    *participants = p;
-    return p;
-}
-
-void printAllParticipants(Participant *participants)
-{
-    Participant *p = participants;
-    while (p != NULL)
-    {
-        char dob[10];
-        sprintf(dob, "%d/%d/%d", p->dob.month, p->dob.day, p->dob.year);
-        printf("-------------------------\n");
-        printf("\tParticipants\n");
-        printf("-------------------------\n");
-        printf("Id:\t\t%d\nName:\t\t%s\nSchool:\t\t%s\nGender:\t\t%c\nDob:\t\t%s\n", p->id, p->name, p->school, p->gender, dob);
-        p = p->next;
-    }
-}
-
-int getParticipantAge(int birthYear)
-{
-    int age = 0;
-    time_t t = time(NULL);
-    struct tm *currentTime = localtime(&t);
-    if (birthYear != 0)
-    {
-        age = (currentTime->tm_year + MIN_YR) - birthYear;
-    }
-    return age;
-}
-
-void printParticipant(Participant *p)
-{
-    char dob[10];
-    sprintf(dob, "%d/%d/%d", p->dob.month, p->dob.day, p->dob.year);
-    printf("Id:\t\t%d\nName:\t\t%s\nSchool:\t\t%s\nGender:\t\t%c\nDob:\t\t%s\n", p->id, p->name, p->school, p->gender, dob);
-}
-
-void destroyParticipant(Participant *p)
-{
-    free(p);
+    p->Register = NULL;
+    p->Destroy = NULL;
+    p->dob = NULL;
 }
