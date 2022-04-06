7aca81514b82d78bb87759b9c04e137958768796
Daneil Greaves <daneilgreaves@gmail.com>
2022-04-06 10:20:44 -0500

    change demo name

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 int main()
 {
     Participant p = InitParticipant();
-    Date dob = {.day = 26, .month = 2, .year = 2007};
-    p.RegisterParticipant(&p, "Daneil Greaves", "Mona High School", 'M', &dob);
+    Date dob = {.day = 1, .month = 1, .year = 2007};
+    p.RegisterParticipant(&p, "John Doe", "Mona High School", 'M', &dob);
     p.DestroyParticipant(&p);
     return 0;
 }
